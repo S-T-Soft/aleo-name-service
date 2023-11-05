8cbae53f0ad8268d0c8955e5d7c82b8f1c6bf334
snowtiger <snowtigersoft@126.com>
2023-11-05 10:57:10 +0800

    Change page title

--- frontend/src/pages/account/index.tsx
+++ frontend/src/pages/account/index.tsx
@@ -19,7 +19,7 @@ const AccountPage: NextPageWithLayout = () => {
   return (
     <>
       <NextSeo
-        title="Aleo Name Service | Account"
+        title="Account | Aleo Name Service"
         description="Aleo Name Service account page"
       />
       <div className="mx-auto w-full px-4 pt-8 pb-14 sm:px-6 sm:pb-20 sm:pt-12 lg:px-8 xl:px-10 2xl:px-0">
