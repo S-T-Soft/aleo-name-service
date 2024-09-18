272e7efef4d642b778951c98717b232c668a22d9
snowtiger <snowtigersoft@126.com>
2024-09-18 12:55:59 +0800

    change indexeddb table name

--- frontend/src/lib/db/index.ts
+++ frontend/src/lib/db/index.ts
@@ -1,15 +1,16 @@
 import Dexie from 'dexie';
 
 const db = new Dexie('ANSDB');
+const tableName = "names_v2"
 
-db.version(2).stores({
-  names: '++id, name, hash, field'
+db.version(1).stores({
+  [tableName]: '++id, name, hash, field'
 });
 
 export async function queryName(query: string) {
   try {
-    const itemByName = await db.table("names").where('name').equals(query).first();
-    const itemByHash = await db.table("names").where('hash').equals(query).first();
+    const itemByName = await db.table(tableName).where('name').equals(query).first();
+    const itemByHash = await db.table(tableName).where('hash').equals(query).first();
     return itemByName || itemByHash || null;
   } catch (error) {
     return null;
@@ -17,20 +18,20 @@ export async function queryName(query: string) {
 }
 
 export async function queryByField(field: string) {
-  return db.table("names").where('field').equals(field).first();
+  return db.table(tableName).where('field').equals(field).first();
 }
 
 export async function saveName(name: string, hash: string, field?: string) {
-  const existingByName = await db.table("names").where('name').equals(name).first();
-  const existingByHash = await db.table("names").where('hash').equals(hash).first();
+  const existingByName = await db.table(tableName).where('name').equals(name).first();
+  const existingByHash = await db.table(tableName).where('hash').equals(hash).first();
   const item = existingByHash || existingByName;
 
   if (item) {
     if (field && item.field !== field) {
-      await db.table("names").update(item.id, {field});
+      await db.table(tableName).update(item.id, {field});
     }
     return;
   }
 
-  await db.table("names").add({name, hash});
+  await db.table(tableName).add({name, hash});
 }
