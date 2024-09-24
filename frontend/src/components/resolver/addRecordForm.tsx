6d3c6ff6c505dcf8ec80ab7d77b999c1bfba0993
snowtiger <snowtigersoft@126.com>
2024-09-24 23:40:30 +0800

    Add address validater

--- frontend/src/components/resolver/addRecordForm.tsx
+++ frontend/src/components/resolver/addRecordForm.tsx
@@ -1,11 +1,12 @@
 import {useANS} from "@/lib/hooks/use-ans";
-import {useState} from "react";
+import {useMemo, useState} from "react";
 import {Status, Record} from "@/types";
 import coinsWithIcons from "@/constants/coinsWithIcons.json";
 import coinsWithoutIcons from "@/constants/coinsWithoutIcons.json";
 import {DynamicAddressIcon} from "@/assets/address/DynamicAddressIcon";
 import Button from "@/components/ui/button";
 import {RefreshIcon} from "@/components/icons/refresh";
+import { getCoderByCoinName } from '@ensdomains/address-encoder'
 
 export const AddRecordForm = ({record, onSuccess}: React.PropsWithChildren<{
   record: Record,
@@ -16,9 +17,49 @@ export const AddRecordForm = ({record, onSuccess}: React.PropsWithChildren<{
   const [content, setContent] = useState("");
   const [setting, setSetting] = useState(false);
   const [status, setStatus] = useState("");
+  const [error, setError] = useState("");
+
+  const disable = useMemo(() => {
+    return !chooseCoin || content === "" || error != "";
+  }, [chooseCoin, content, error])
+
+  const validateCryptoAddress = ({
+    coin,
+    address,
+  }: {
+    coin: string
+    address: string | undefined
+  }) => {
+    try {
+      if (!address) return 'Address Required'
+      const _address = address || ""
+      const coinTypeInstance = getCoderByCoinName(coin)
+      coinTypeInstance.decode(_address)
+      return true
+    } catch (e: any) {
+      return 'Invalid Address'
+    }
+  }
+
+  const validateAddress = () => {
+    if (!chooseCoin || content === "") {
+      setError("");
+      return;
+    }
+    const validate = validateCryptoAddress({coin: chooseCoin, address: content});
+    if (validate != true) {
+      setError(validate);
+    } else {
+      setError("")
+    }
+  }
 
   const handleSetting = async (event: any) => {
     if (!content || content.length == 0) {
+      return;
+    }
+    validateAddress();
+    if (error != "") {
       return;
     }
     await setResolverRecord(record, chooseCoin, content, (running: boolean, status: Status) => {
@@ -36,11 +77,12 @@ export const AddRecordForm = ({record, onSuccess}: React.PropsWithChildren<{
     if (chooseCoin !== coin) {
       setChooseCoin(coin);
       setContent("");
+      setError("");
     }
   }
 
   return <div className="w-full mt-5">
-    <div className="flex overflow-x-auto mb-3 overflow-y-hidden whitespace-nowrap py-2">
+    <div className="flex overflow-x-auto mb-5 overflow-y-hidden whitespace-nowrap py-2">
       {coinsWithIcons.map((coin) => (
         <div
           key={coin}
@@ -60,7 +102,8 @@ export const AddRecordForm = ({record, onSuccess}: React.PropsWithChildren<{
         </div>
       ))}
     </div>
-    <div className="relative flex w-full rounded-full mt-5">
+    {error && <div className={`font-bold align-text-bottom w-full text-red-500 block text-right pr-5`}>{error}</div>}
+    <div className="relative flex w-full rounded-full">
       {chooseCoin &&
           <span className="absolute flex h-full w-15 items-center justify-center ltr:pl-2 left-2 dark:text-white">
                   <DynamicAddressIcon name={chooseCoin} showDefault={false} className="w-6 h-6"/>
@@ -68,17 +111,17 @@ export const AddRecordForm = ({record, onSuccess}: React.PropsWithChildren<{
               </span>}
       <label className="flex w-full items-center ">
         <input
-          className="h-16 w-full appearance-none rounded-full border-2 border-gray-200 py-1 text-lg tracking-tighter text-gray-900 outline-none transition-all placeholder:text-gray-600 hover:border-teal focus:border-aquamarine px-24 dark:border-gray-600 dark:bg-light-dark dark:text-white dark:placeholder:text-gray-500"
+          className={`${error ? "border-red-500": "border-gray-200 dark:border-gray-600"} h-16 w-full appearance-none rounded-full border-2 py-1 text-lg tracking-tighter text-gray-900 outline-none transition-all placeholder:text-gray-600 hover:border-teal focus:border-aquamarine px-24 dark:bg-light-dark dark:text-white dark:placeholder:text-gray-500`}
           value={content}
           placeholder={chooseCoin ? `Please enter the ${chooseCoin.toUpperCase()} address` : "Please select a coin first"}
           onChange={(event) => setContent(event.currentTarget.value)}
+          onBlur={validateAddress}
           autoComplete="off"
         />
       </label>
       <span
         className="absolute flex h-full w-15 cursor-pointer items-center justify-center text-gray-900 hover:text-gray-900 ltr:right-2 ltr:pr-2 rtl:left-2 rtl:pl-2 dark:text-white ">
-                {!setting && <Button className={(!chooseCoin || content === "") ? "bg-gray-700" : ""}
-                                     onClick={handleSetting} disabled={!chooseCoin || content === ""}>Set</Button>}
+                {!setting && <Button className={disable ? "bg-gray-700" : ""} onClick={handleSetting} disabled={disable}>Set</Button>}
         {setting && <Button color={"gray"} disabled={true}><RefreshIcon
             className="inline text-aquamarine motion-safe:animate-spin"/> {status}</Button>}
               </span>
--- frontend/src/pages/account/transfer.tsx
+++ frontend/src/pages/account/transfer.tsx
@@ -3,6 +3,7 @@ import {Status} from "@/types";
 import Button from "@/components/ui/button";
 import {RefreshIcon} from "@/components/icons/refresh";
 import tlds from "@/config/tlds";
+import {useANS} from "@/lib/hooks/use-ans";
 
 export default function Transfer({name, transfer, setTriggerRecheck}: React.PropsWithChildren<{
   name: string,
@@ -10,6 +11,7 @@ export default function Transfer({name, transfer, setTriggerRecheck}: React.Prop
   setTriggerRecheck: CallableFunction
 }>) {
   const tldName = tlds[0].name;
+  const {matchTld} = useANS();
   const [transferring, setTransferring] = useState(false);
   const [transferStatus, setTransferStatus] = useState("Transferring");
   const [recipient, setRecipient] = useState("");
@@ -19,9 +21,23 @@ export default function Transfer({name, transfer, setTriggerRecheck}: React.Prop
     setRecipient(event.currentTarget.value);
   };
 
-  const handleTransfer = async (event: any) => {
+  const validateAddress = () => {
     if (!recipient || recipient.length == 0) {
-      setError("Recipient address is required");
+      setError("Recipient required");
+      return;
+    }
+    const matchedTld = matchTld(recipient);
+    const isAleoAddress = recipient.startsWith("aleo1") && recipient.length > 60;
+    if (!matchedTld && !isAleoAddress) {
+      setError("Invalid recipient");
+      return;
+    }
+    setError("");
+  }
+
+  const handleTransfer = async (event: any) => {
+    validateAddress();
+    if (error != "") {
       return;
     }
     setError("");
@@ -35,13 +51,15 @@ export default function Transfer({name, transfer, setTriggerRecheck}: React.Prop
   }
 
   return <>
-    <div className="relative flex w-full rounded-full mt-5">
+    {error && <div className={`font-bold align-text-bottom w-full text-red-500 block text-right pr-5 mt-5`}>{error}</div>}
+    <div className={`relative flex w-full rounded-full ${!error && "mt-5"}`}>
       <label className="flex w-full items-center ">
         <input
           className={`h-16 w-full appearance-none rounded-full border-2 py-1 text-lg tracking-tighter text-gray-900 outline-none transition-all placeholder:text-gray-600 hover:border-teal focus:border-aquamarine ltr:pr-24 ltr:pl-8 rtl:pl-24 rtl:pr-8 dark:bg-light-dark dark:text-white dark:placeholder:text-gray-500 sm:ltr:pr-24 sm:rtl:pl-24 xl:ltr:pr-24 xl:rtl:pl-24 ${error ? 'border-red-500' : 'border-gray-200 dark:border-gray-600'}`}
           value={recipient}
           placeholder={`Address(aleo***) or Public ANS(***.${tldName})`}
           onChange={onChange}
+          onBlur={validateAddress}
           autoComplete="off"
         />
       </label>
