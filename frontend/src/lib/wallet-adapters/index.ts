fb6efd265b00cbfc4e36c1e1d12566226a1d2724
snowtiger <snowtigersoft@126.com>
2024-12-02 22:58:10 +0800

    Fix typo

--- frontend/src/lib/wallet-adapters/index.ts
+++ frontend/src/lib/wallet-adapters/index.ts
@@ -1,6 +1,6 @@
 export {FoxWalletAdapter} from "./wallet-adapter/foxwallet";
 export {PuzzleWalletAdapter} from "./wallet-adapter/puzzle-wallet";
 export {SoterWalletAdapter} from "./wallet-adapter/soter-wallet";
-export {LeoWalletAdapter} from "./wallet-adapter/let-wallet";
+export {LeoWalletAdapter} from "./wallet-adapter/leo-wallet";
 export {configureConnection as configureConnectionForPuzzle} from "@puzzlehq/sdk";
 export {AvailWalletAdapter} from "./wallet-adapter/avail-wallet";
