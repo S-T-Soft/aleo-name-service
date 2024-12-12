5c87edb9381fe1c4fffc5373dba1fc58073f4dd0
snowtiger <snowtigersoft@126.com>
2024-12-12 23:45:55 +0800

    Set privateFee to false when come from coinbase quest

--- frontend/src/pages/quest/coinbase.tsx
+++ frontend/src/pages/quest/coinbase.tsx
@@ -2,15 +2,18 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useTrace } from '@/lib/hooks/use-trace';
 import DOMPurify from "dompurify";
+import {usePrivateFee} from "@/lib/hooks/use-private-fee";
 
 const CoinbaseQuest = () => {
   const router = useRouter();
+  const {setPrivateFee} = usePrivateFee();
   const { setCbUUID } = useTrace();
 
   useEffect(() => {
     const { uuid, next } = router.query;
     if (uuid) {
       setCbUUID(uuid as string);
+      setPrivateFee(false);
       if (next && ((next as string).startsWith('/') || (next as string).startsWith(location.origin))) {
         const sanitizedNext = DOMPurify.sanitize(next as string);
         router.push(sanitizedNext);
